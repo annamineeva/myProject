5f07e56921889a4a15841e7bade47065f241b802
Ardor <zvetok.lolo2@gmail.com>
2023-10-26 15:04:26 +0300

    Initial commit

---
+++ test/src/App.js
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <div>fgdh</div>
+      <h1>ryhs56rtyh</h1>
+     
+      
+    </div>
+  )
+    
+}
+
+export default App;
---
+++ test/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Navbar() {
+    return (
+        <div>
+            <div>
+                <ol className="Navbar">
+                    <li className="li">oui</li>
+                </ol>
+            </div>
+        </div>
+    )
+}
